92c611dde2b0b4ecdebd0e55dfd7ac98ea763182
Lilly Hyatt <lillyhyatt@Lillys-MacBook-Pro.local>
2023-04-19 15:58:51 +0100

    added serverless

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,20 @@
 const messageNode = document.getElementById('message');
 
+const render = (message = "", DOMnode = messageNode) => {
+    DOMnode.textContent = message;
+}
+
+const getData = async () => {
+ try{
+const response = await fetch('/.netlify/functions/hello-world')
+if(!response.ok) throw response;
+const data = await response.json();
+console.log(data);
+render(data.message);
+ } catch (err){
+altert ('Error fetching check onsole')
+console.log(err);
+ }
+}
+
+getData();
