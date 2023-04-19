33f6460e5e1e81d0df51a17c0c7f23abd0ca7d3f
Lilly Hyatt <lillyhyatt@Lillys-MacBook-Pro.local>
2023-04-19 17:15:48 +0100

    cars api added

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,26 @@
 const messageNode = document.getElementById('message');
 
-const render = (message = "", DOMnode = messageNode) => {
-    DOMnode.textContent = message;
+const render = (cars = [], DOMnode = messageNode) => {
+    const frag = document.createDocumentFragment();
+    for (const car of cars) {
+        const li = document.createElement('li');
+        li.innerHTML = `<img src="${car.avatar_url}"> <span> ${car.name} (${car.bhp}</span>`
+        frag.append(li)
+    }
+    DOMnode.append(frag);
 }
 
 const getData = async () => {
- try{
-const response = await fetch('/.netlify/functions/hello-world')
-if(!response.ok) throw response;
-const data = await response.json();
-console.log(data);
-render(data.message);
- } catch (err){
-altert ('Error fetching check onsole')
-console.log(err);
- }
+    try {
+        const response = await fetch('/.netlify/functions/read-all-cars')
+        if (!response.ok) throw response;
+        const cars = await response.json();
+        console.log(cars);
+        render(cars);
+    } catch (err) {
+        altert('Error fetching check onsole')
+        console.log(err);
+    }
 }
 
 getData();
